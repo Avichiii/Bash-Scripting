a050bb6da516131d7848c4c056dfdd06719c4c19
Abhijit Dey <87671060+Avichiii@users.noreply.github.com>
2023-09-16 12:58:54 +0530

    Update pingsweep.sh
    minor changes in for loop.

--- pingsweep.sh
+++ pingsweep.sh
@@ -6,7 +6,8 @@ echo "you forgot an IP Address"
 echo "Syntex: ./pingsweep 192.168.29"
 
 else 
-    for ip in `seq 1 254`; do
+    for ip in `seq 1 254`; 
+    do
         ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
     done
 fi
